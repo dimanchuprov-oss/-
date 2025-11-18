79a4b02eac49fdc1118b336ec81114a342ba1bef
dimanchuprov-oss <diman.chuprov@gmail.com>
2025-11-18 17:36:00 +0300

    Update arr1.go

--- arr1.go
+++ arr1.go
@@ -7,5 +7,6 @@ import (
 func main() {
 	b := [7]string{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}
 	c := b[1:]
-	fmt.Println(c, len(b), cap(b))
+	fmt.Println(c, len(b), cap(b), len(c))
 }
+
