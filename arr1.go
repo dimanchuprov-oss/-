811bc39197021258633d417050db76ba13c47d6b
dimanchuprov-oss <diman.chuprov@gmail.com>
2025-11-18 17:21:03 +0300

    Add files via upload

---
+++ arr1.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	b := [7]string{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}
+	c := b[1:]
+	fmt.Println(c, len(b), cap(b))
+}
