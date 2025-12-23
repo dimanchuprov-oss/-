2a72a5d28f64e8d69667ada1058cf87639e1d80c
dimanchuprov-oss <diman.chuprov@gmail.com>
2025-12-23 16:58:18 +0300

    Update words.go

--- words.go
+++ words.go
@@ -1,9 +1,32 @@
-package downloads
 package main
 
 import (
-	"html/template"
-	"math/rand"
-	"net/http"
-	"time"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Введите текст: ")
+	text, _ := reader.ReadString('\n')
+	lower := strings.ToLower(text)
+	delimiters := " .,!?;:()[]{}<>\"'`-–—\t\n\r"
+	for _, delim := range delimiters {
+		text = strings.ReplaceAll(lower, string(delim), " ")
+	}
+	
+	words := strings.Fields(lower)
+	
+	uniqueWords := make(map[string]bool)
+	for _, word := range words {
+		uniqueWords[word] = true
+	}
+	var result []string
+	for word := range uniqueWords {
+		result = append(result, word)
+	}
+
+	fmt.Println("Уникальные слова:", result)
+}
